87a1baf5c541aa0ae8267a97f07b8ffd89c43e4e
Florian <dark.joosh@gmail.com>
2025-05-03 13:58:30 +0200

    Add Product Classes

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -1,4 +1,28 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long productId;
+    private String productName;
+    private String productDescription;
+    private Integer productQuantity;
+    private double productPrice;
+    private double specialPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
 }
